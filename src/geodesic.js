6c9e8ea897b68022cb2e8fb8f520d980d7745422
Bassel Saleh <basselsaleh@Bassels-MacBook-Pro.local>
2022-08-12 14:07:32 -0400

    work in progress on particle trail

--- src/geodesic.js
+++ src/geodesic.js
@@ -42,17 +42,48 @@ var p_y = two.height / 2;
 var particle = two.makeCircle(p_x, p_y, p_r);
 particle.fill = '#000000'
 
-two.bind('update', update)
-two.play();
+// make a curve to trail the particle
+var points = []
+for(var k=0; k < 5; k++)
+    points.push(new Two.Anchor(p_x, p_y - k*20))
+var trail = two.makeCurve(points, true)
+trail.linewidth = 2;
+trail.scale = 1.75;
+trail.noFill();
+trail.stroke = 'rgba(255, 0, 0, 0.5)';
 
 // time variable for evolution
 var t = 0;
 
+var orbit_radius = two.width / 4;
+
+// keep track of last 5 points in particle orbit
+var particle_points_x = [];
+var particle_points_y = [];
+
+two.bind('update', update)
+two.play();
+
 // this is called every frame by two.play()
 function update(frameCount) {
     t += 0.1;
-    particle.translation.x = p_x + 50*Math.cos(t);
-    particle.translation.y = p_y + 50*Math.sin(t);
+    var new_x = (p_x*2) + orbit_radius * Math.cos(t);
+    var new_y = p_y - orbit_radius * Math.sin(t);
+    document.write(new_x);
+    particle_points_x.push(new_x);
+    particle_points_y.push(new_y);
+
+    particle.translation.x = new_x;
+    particle.translation.y = new_y;
+
+    // update trail points
+    for(var i=0; i < particle_points_x.length; i++) {
+        var p = points[i];
+        var trail_x = particle_points_x[particle_points_x.length - 1 - i];
+        var trail_y = particle_points_y[particle_points_y.length - 1 - i];
+        p.x = trail_x;
+        p.y = trail_y;
+    }
 }
 
 }
